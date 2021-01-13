c63ef7650a5bc7a05ce99ab844588762fae6de43
Nicole Dreon <dreon.nicole@gmail.com>
2021-01-13 13:07:36 -0500

    removed default styles from nav a tags

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 import { HomeComponent } from './home/home.component';
 import { ProjectsComponent } from './projects/projects.component';
 
@@ -8,6 +9,7 @@ const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'projects', component: ProjectsComponent },
+  { path: 'contact', component: ContactComponent },
   { path: '**', pathMatch: 'full', component: HomeComponent },
 ];
 
